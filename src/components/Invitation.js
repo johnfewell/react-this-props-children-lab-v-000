4510980a7e3d491ba9730bc875f09a7c1d21cd71
meq <fewell@gmail.com>
2018-05-13 17:16:50 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,22 @@
-// Code Invitation Component Here
+// // Code Invitation Component Here
+// In the components/Invitation.js file, create an Invitation React component.
+// This component renders out an h1 with the text 'You've been invited!'
+// After this h1, render out any children that are passed into the Invitation component. This allows us to customize the message.
+import React from 'react';
+
+export default class Invitation extends React.Component {
+  render() {
+    const childrenOfInvitation = React.Children.map(this.props.children, child => {
+      return (
+        <div>{child}</div>
+      );
+    });
+
+    return (
+      <div>
+      <h1>You{"'"}ve been invited!</h1>
+        {childrenOfInvitation}
+      </div>
+    );
+  }
+}
